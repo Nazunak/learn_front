d1036b30d6099c4e4fe39b63a6a21ac4c7a54d7c
pavlo <osnova.reiste@gmail.com>
2023-07-25 08:56:23 +0300

    work monday cowo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// бургер меню 
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu, .header__burger-span').toggleClass('active');
+        $('body').toggleClass('block');
+    });
+});
+
+// кінець бургер меню 
