562608acc449a2bdc7494ce1488a28146e6ae6f2
pavlo <osnova.reiste@gmail.com>
2023-07-29 15:54:10 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -17,13 +17,65 @@ new Swiper('.image-slider', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
+    // навігація
+    pagination: {
+        el: '.swiper-pagination',
+        // буллети
+        clickable: true,
+        // буллети динамічні
+        dynamicBullets: true,
+        // буллети кастомні
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+        // // фракція
+        // type: 'fraction',
+        // // фракція кастомна
+        // renderFraction: function(currentClass, totalClass) {
+        // return ' Фото <span class="' + currentClass + '"></span>' +
+        //     ' з ' +
+        //     '<span class="' + totalClass + '"></span>';
+        // // прогресбар
+        // type: 'progresbar', 
+        },
+    // кількість відображення картинок на одному слайді
     slidesPerView: 1,
+    // // скролбар
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //     draggable: true
+    // },
+    // курсор перетаскування
+     grabCursor: true,
+    // слайд мишкою
+    mousewheel: {
+        sensitivity: 1,
+    },
+    // // відступ між слайдами
+    // spaceBetween: 30,
+    // // кількість пролистуюмих слайдів
+    // slidePerGroup: 1,
+    // // активний слайд по центру
+    // centeredSlides: true,
 });
+
 
 // ініціалізація слайдера 
 
 
 
+// // скролл
+// scrollbar: {
+//     el: '.swiper-scrollbar',
+//     draggable: true
+// },
+// // курсор перетаскування
+// grabCursor: true,
+// // слайд мишкою
+// mousewheel: {
+//     sensitivity: 1,
+// },
+// // кількість видимих слайдів
 
 
 
